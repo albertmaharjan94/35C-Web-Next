282d2a592a9d2b86b53010fb17e82f5feb4adbec
albert <albert.maharjan94@gmail.com>
2026-02-04 13:19:01 +0545

    feb 4

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -6,7 +6,9 @@ export const API = {
         LOGIN: '/api/auth/login',
         REGISTER: '/api/auth/register',
         WHOAMI: '/api/auth/whoami',
-        UPDATEPROFILE: '/api/auth/update-profile'
+        UPDATEPROFILE: '/api/auth/update-profile',
+        REQUEST_PASSWORD_RESET: '/api/auth/request-password-reset',
+        RESET_PASSWORD: (token: string) => `/api/auth/reset-password/${token}`,
     },
     ADMIN: {
         BLOGS: {
